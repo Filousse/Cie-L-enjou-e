1943a3f22c822b5689bc46f26e7243c08aacb57a
Thibault Brasseur <43286379+Filousse@users.noreply.github.com>
2019-12-02 17:29:54 +0100

    js file
    scolling path event whit jquery fr the only web site page

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop()) {
+              // $("#logoSection").addClass("fadeOut");
+              $("#header").addClass("fadeIn");
+            }
+        });
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 50 ) {
+                $("#cieSection").addClass("fadeIn");
+                $(".Wiggle").addClass("wiggle");
+              }
+          });
+          $(window).scroll(function () {
+              if ($(this).scrollTop() > 620) {
+                  $("#distributionSection").addClass("zoomer");
+                } else {
+                  $("#distributionSection").addClass("fadeIn").removeClass();
+                }
+            });
+
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 2000) {
+                    $("#creationSection").addClass("slideRight");
+                  } else {
+                    $("#creationSection").addClass("fadeIn").removeClass();
+                  }
+              });
+              $(window).scroll(function () {
+                  if ($(this).scrollTop() > 3300) {
+                      $("#perso1").addClass("slideRight");
+                      $("#perso2").addClass("slideLeft");
+                    }
+              });
+
+
+
+
+
+
+
+      // $('#animatedElement').click(function() {
+      //   $(this).addClass("barrelRoll");
+      // });
+});
